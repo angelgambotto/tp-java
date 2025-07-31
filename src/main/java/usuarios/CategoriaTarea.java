e04ee113fece931938c01c5a54608a0f3fe18a86
IriDev <irirepupilli123@gmail.com>
2025-07-31 16:53:29 -0300

    create class categoriaTarea

---
+++ src/main/java/usuarios/CategoriaTarea.java
@@ -0,0 +1,35 @@
+package usuarios;
+
+public class CategoriaTarea {
+	private int id;
+	private String nombre; 
+	private String descripcion;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public CategoriaTarea(int id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+	
+	
+
+}
